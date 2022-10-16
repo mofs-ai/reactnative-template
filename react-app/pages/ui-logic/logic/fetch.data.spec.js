f78c392da33834ecb833f7fe0ff3452c9a5e4a1f
hs.choi <hs.choi@actbase.io>
2022-10-16 21:35:27 +0900

    fix test case

--- react-app/pages/ui-logic/logic/fetch.data.spec.js
+++ react-app/pages/ui-logic/logic/fetch.data.spec.js
@@ -2,9 +2,9 @@ import {describe, expect, test} from '@jest/globals';
 import fetchLogic from './fetch.data';
 
 describe('fetch module', () => {
-  it('fetch url', () => {
+  it('fetch url', async () => {
     expect(
-      fetchLogic.getData('https://reqres.in/api/users/2').then(res => {
+      await fetchLogic.getData('https://reqres.in/api/users/2').then(res => {
         console.log('[result]\n', res);
       }),
     ).not.toBeNull();
